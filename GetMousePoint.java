08ef96056a3dba7497fca6485dcdb119455edd3e
zackowens18 <34559775+zackowens18@users.noreply.github.com>
2017-12-25 00:45:59 -0500

    Add files via upload

--- GetMousePoint.java
+++ GetMousePoint.java
@@ -5,10 +5,11 @@ import java.awt.Robot;
 public class GetMousePoint{
     public static void main(String[] args) throws Exception {
 		Robot robot = new Robot();
+		int i = 0;
         while (true) {
 			MouseInfo.getPointerInfo();
-			System.out.println(MouseInfo.getPointerInfo().getLocation());
-
+			System.out.println(MouseInfo.getPointerInfo().getLocation()+ " iteration: " + i);
+			i++;
             try {
 			Thread.sleep(1000);
 			} catch (InterruptedException e) {
